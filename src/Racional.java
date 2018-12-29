c8342d5c2a56a22631c6095111f3230c35c175f3
joao <joao182cx@gmail.com>
2018-12-28 23:23:50 -0200

    Criação das variáveis numerador e denominador.

--- src/Racional.java
+++ src/Racional.java
@@ -1,6 +1,22 @@
 
 public class Racional {
 	
+	private int numerador;
+	private int denominador;
 	
-
+	
+	public int getNumerador() {
+		return numerador;
+	}
+	public void setNumerador(int numerador) {
+		this.numerador = numerador;
+	}
+	public int getDenominador() {
+		return denominador;
+	}
+	public void setDenominador(int denominador) {
+		this.denominador = denominador;
+	}
+	
+	
 }
